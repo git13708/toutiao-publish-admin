2b369790c400314164ba66c3aab9672d9fbb56f4
git13708 <wy13708@126.com>
2020-11-11 09:37:05 +0800

    项目初始化完成

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,35 @@
     <h2>黑马头条内容发布系统</h2>
     <!-- 路由出口 -->
     <router-view/>
+    <el-button>默认按钮</el-button>
+    <el-button type="danger">默认按钮</el-button>
+    <el-button type="info" round>默认按钮</el-button>
+    <el-button type="info" circle icon="el-icon-edit"></el-button>
+    <el-row>
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
+    <el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      num: 1
+    }
+  },
+  methods: {
+    handleChange (value) {
+      console.log(value)
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,9 +5,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 加载Element组件库
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 // 加载全局样式文件
 import './styles/index.less'
+
+// 全局注册Element 组件库
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
